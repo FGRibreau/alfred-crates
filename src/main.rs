b3b3de99f07364ef4febb760cf753120759ce637
FG Ribreau <github@fgribreau.com>
2017-01-29 17:22:46 +0100

    feat(Cargo): v1.0.0

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,16 @@ const HOST: &'static str = "https://crates.io";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     assert!(args.len() > 0, "Usage: ./alfred-crates [query]");
-
     let ref query = args[1];
-
     let mut registry = Registry::new(String::from(HOST), None);
-
     match registry.search(&query, 10) {
         Ok((crates, _)) => {
             println!("<?xml version=\"1.0\"?>");
             println!("<items>");
 
             // https://www.alfredapp.com/help/workflows/inputs/script-filter/xml/
-            crates.iter().inspect(|krate| println!("<item arg=\"{}/crates/{}\" type=\"url\"><title>{}</title><subtitle>{}</subtitle></item>", HOST, krate.name,krate.name, krate.description.clone().unwrap_or(String::from("OK")))).collect::<Vec<_>>();
+            crates.iter().inspect(|krate| println!("<item arg=\"{}/crates/{}\" type=\"url\"><title>{}</title><subtitle>{}</subtitle></item>", HOST, krate.name,krate.name, krate.description.clone().unwrap_or(String::from("")))).collect::<Vec<_>>();
             println!("</items>");
             return ();
         },
