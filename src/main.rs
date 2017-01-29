fa07a5214913f5c1402ad86af6e3ee1f3fb23bf4
FG Ribreau <github@fgribreau.com>
2017-01-29 17:40:43 +0100

    fix(main.rs): add escaping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate crates_io;
+extern crate xml;
 
 use std::env;
+use xml::escape::escape_str_pcdata;
 use crates_io::{Registry, Crate};
 
 const HOST: &'static str = "https://crates.io";
@@ -28,6 +30,9 @@ fn workflow_output(crates: Vec<Crate>){
     println!("<?xml version=\"1.0\"?>");
     println!("<items>");
     // https://www.alfredapp.com/help/workflows/inputs/script-filter/xml/
-    crates.iter().inspect(|krate| println!("<item arg=\"{}/crates/{}\" type=\"url\"><title><![CDATA[{}]]></title><subtitle><![CDATA[{}]]></subtitle></item>", HOST, krate.name,krate.name, krate.description.clone().unwrap_or(String::from("")))).collect::<Vec<_>>();
+    crates.iter().inspect(|krate| {
+        let escaped_name = escape_str_pcdata(&krate.name);
+        println!("<item arg=\"{}/crates/{}\" type=\"url\"><title><![CDATA[{}]]></title><subtitle><![CDATA[{}]]></subtitle></item>", HOST, escaped_name,escaped_name, escape_str_pcdata(&krate.description.clone().unwrap_or(String::from(""))))
+    }).collect::<Vec<_>>();
     println!("</items>");
 }
